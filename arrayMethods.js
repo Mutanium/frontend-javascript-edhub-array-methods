3e91d31db9052bbd5880bc90054a6d4a26b1a2ab
Mutanium <info@gaetan.nl>
2021-05-17 00:05:43 +0200

    array methods uitgevoerd

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,16 +18,30 @@ const inventors = [
 // 1. Filter de lijst op uitvinders die geboren zijn in de 16e eeuw
 // Verwachte uitkomst:
 // [{ first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 }, { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 }]
-
+const sixteenthCent = inventors.filter((inventor)=> {
+  return inventor.year < 1600 && inventor.year >1500;
+})
+console.log(sixteenthCent);
 
 
 // 2. Maak een array met daarin alle geboortejaren van de uitvinders
 // Verwachte uitkomst: [1879, 1643, 1564, 1867, 1571, 1473, 1858, 1898, 1815, 1855, 1878, 1847];
+let birthYear = [];
+for (let i =0; i< inventors.length; i++) {
+  birthYear.push(inventors[i].year);
+}
+console.log(birthYear);
 
 
 
 // 3. Maak een array met daarin alle volledige namen van de uitvinders (dus voor- en achternaam als één string)
 // Verwachte uitkomst: [ 'Albert Einstein', 'Isaac Newton', 'Galileo Galilei', 'Marie Curie', 'Johannes Kepler', 'Nicolaus Copernicus', 'Max Planck', 'Katherine Blodgett', 'Ada Lovelace', 'Sarah E. Goode', 'Lise Meitner', 'Thomas Edison']
+
+let fullName = [];
+for (let i =0; i< inventors.length; i++) {
+  fullName.push(inventors[i].first + " " + inventors[i].last);
+}
+console.log(fullName);
 
 
 
@@ -48,6 +62,17 @@ const inventors = [
 //   { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 }
 // ]
 
+inventors.sort((a,b)=>{
+  if(a.year>b.year){
+    return 1;
+  }
+  if(a.year<b.year){
+    return -1;
+  }
+  return 0;
+})
+console.log(inventors);
+
 
 
 // 5. Sorteer de uitvinders op hoeveel jaren ze geleefd hebben, van langste leven naar kortste leven
@@ -67,8 +92,23 @@ const inventors = [
 // { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 }
 // ]
 
-
+inventors.sort((a,b)=>{
+  if(a.passed-a.year>b.passed-b.year){
+    return -1;
+  }
+  if(a.passed-a.year<b.passed-b.year){
+    return 1;
+  }
+  return 0;
+})
+console.log(inventors);
 
 
 // 6. Vind de gegevens over de uitvinder wiens achternaam 'Edison' is.
 // Verwachte uitkomst: { first: 'Thomas', last: 'Edison', year: 1847, passed: 1931 }
+const specificInventor = inventors.find((inventor)=>{
+  return inventor.last === "Edison";
+});
+console.log(specificInventor);
+
+//let op dat je bij de retur de geinroduceerde enkelvoud gebruikt!
